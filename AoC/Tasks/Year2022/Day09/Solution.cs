effac519fe5cf23d0bc8c8118d4eb9ae98e524d3
Sebastian Löfstrand <sebastian.lofstrand@nordicstation.com>
2022-12-22 19:07:10 +0100

    Updates

---
+++ AoC/Tasks/Year2022/Day09/Solution.cs
@@ -0,0 +1,70 @@
+﻿using AoC.Helpers;
+
+namespace AoC.Tasks.Year2022.Day09;
+
+public class Solution : ISolver
+{
+    private readonly List<Move> _input;
+
+    public Solution(string input)
+    {
+        _input = InputHelper
+            .ToStringList(input)
+            .ToTupleList(" ")
+            .Select(x => new Move() { Direction = x.Item1, Distance = int.Parse(x.Item2) })
+            .ToList();
+    }
+
+    public object PartOne()
+    {
+        return VisitedRopeKnots(2);
+    }
+
+    public object PartTwo()
+    {
+        return VisitedRopeKnots(10);
+    }
+
+    public int VisitedRopeKnots(int knots)
+    {
+        var rope = new (int X, int Y)[knots];
+        var visited = new HashSet<(int, int)>();
+
+        foreach (var move in _input)
+        {
+            for (int i = 0; i < move.Distance; i++)
+            {
+                rope[0] = move.Direction switch
+                {
+                    "U" => (rope[0].X, --rope[0].Y),
+                    "D" => (rope[0].X, ++rope[0].Y),
+                    "R" => (++rope[0].X, rope[0].Y),
+                    "L" => (--rope[0].X, rope[0].Y),
+                    _ => throw new ArgumentOutOfRangeException()
+                };
+
+                for (int j = 1; j < rope.Length; j++)
+                {
+                    var xDist = rope[j - 1].X - rope[j].X;
+                    var yDist = rope[j - 1].Y - rope[j].Y;
+
+                    if (Math.Abs(xDist) > 1 || Math.Abs(yDist) > 1)
+                    {
+                        rope[j].X += Math.Sign(xDist);
+                        rope[j].Y += Math.Sign(yDist);
+                    }
+                }
+
+                visited.Add(rope.Last());
+            }
+        }
+
+        return visited.Count;
+    }
+}
+
+public class Move
+{
+    public int Distance { get; set; }
+    public string Direction { get; set; } = string.Empty;
+}
---
+++ AoC/Tasks/Year2022/Day10/Solution.cs
@@ -0,0 +1,89 @@
+﻿using AoC.Helpers;
+
+namespace AoC.Tasks.Year2022.Day10;
+
+public class Solution : ISolver
+{
+    private readonly List<Instruction> _list;
+
+    public Solution(string input) => _list = InputHelper.ToStringList(input)
+        .Select(x =>
+        {
+            var strSplit = x.Split(" ");
+            var command = strSplit[0];
+            var value = (strSplit.Length > 1) ? int.Parse(strSplit[1]) : 0;
+            return new Instruction { Command = command, Value = value };
+        })
+        .ToList();
+
+    public object PartOne()
+    {
+        var cycles = GetCycles();
+
+        var index = 20;
+        var sum = 0;
+        while (index <= 220)
+        {
+            var element = cycles.ElementAt(index);
+            var total = (element * (index));
+            Console.WriteLine($"{index} * {element} = {total}");
+            sum += total;
+            index += 40;
+        }
+
+        return sum;
+    }
+
+    public object PartTwo()
+    {
+        var cycles = GetCycles();
+        var drawnCycles = new List<List<string>>();
+        var tempList = Enumerable.Range(0, 40).ToList().Select(x => ".").ToList();
+        for (int i = 0; i < cycles.Count; i++)
+        {
+            if (cycles.Count % 40 == 0)
+            {
+                drawnCycles.Add(tempList);
+                tempList = Enumerable.Range(0, 40).ToList().Select(x => ".").ToList();
+            }
+
+            // tempList.ElementAt(i)
+
+        }
+        
+        drawnCycles.ForEach(cList =>
+        {
+            cList.ForEach(Console.Write);
+            Console.WriteLine();
+        });
+        return int.MinValue;
+    }
+
+    private List<int> GetCycles()
+    {
+        int x = 1;
+        var cycles = new List<int> { x };
+        _list.ForEach(instruction =>
+        {
+            switch (instruction.Command)
+            {
+                case "noop":
+                    cycles.Add(x);
+                    break;
+                case "addx":
+                    cycles.Add(x);
+                    cycles.Add(x);
+                    break;
+            }
+
+            x += instruction.Value;
+        });
+        return cycles;
+    }
+}
+
+public class Instruction
+{
+    public string Command { get; set; }
+    public int Value { get; set; }
+}
